c6c7e945e1152e5a0568f89342f268b367c537a6
Your Name <kalaitzidispaul@gmail.com>
2022-05-19 16:07:58 -0400

    set up MOdels with mongoose

---
+++ Models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model, Types } = require('mongoose')
+const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new Schema({
+    // set a custom id to avoid confusion with parent "thoughtSchema"
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId
+    },
+    reactioonBody: {
+        type: String,
+        required: true,
+        max: 280
+    },
+    username: {
+        type: String, 
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+    }
+},
+{
+    toJSON: {
+        getters: true
+    }
+}
+)
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min: 1,
+            max: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+        username: {
+            // (The user that created this thought)
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
---
+++ Models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model, Types} = require('mongoose')
+
+
+// create schema for user
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            // trimmed set to true
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [UserSchema]
+    }
+)
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.length
+})
+
+// required email validator?
+// const User = db.model('User', UserSchema)
+// const user = new UserSchema();
+
+// let error;
+// try {
+//   await user.save();
+// } catch (err) {
+//   error = err;
+// }
+    
+// assert.equal(error.errors['email'].message,
+//   'Path `name` is required.');
+
+// error = cat.validateSync();
+// assert.equal(error.errors['email'].message,
+//   'Path `email` is required.');
+
+module.exports = User;
